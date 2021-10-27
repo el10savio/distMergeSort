2477c423e89f8d1ff10c4bb77a76ea0896c0e2ac
el10savio <el10savio96@gmail.com>
2021-10-27 12:57:28 +0530

    Added Sort Handler

--- sort/sort.go
+++ sort/sort.go
@@ -1,1 +1,16 @@
 package sort
+
+import "sort"
+
+// Sort ...
+func Sort(elements []int) []int {
+	if len(elements) == 0 {
+		return []int{}
+	}
+	return sliceSort(elements)
+}
+
+// sliceSort ...
+func sliceSort(elements []int) []int {
+	return sort.IntSlice(elements)
+}
