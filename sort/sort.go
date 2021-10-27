db7ff594902ed9142481b3fdadfc4531ec1837ae
el10savio <el10savio96@gmail.com>
2021-10-27 13:05:22 +0530

    Init merge Function

--- sort/sort.go
+++ sort/sort.go
@@ -14,3 +14,22 @@ func Sort(elements []int) []int {
 func sliceSort(elements []int) []int {
 	return sort.IntSlice(elements)
 }
+
+// merge ...
+func merge(elements1, elements2 []int) []int {
+	// Check if either array is empty - if so return the other
+
+	// Get bigger array size to allocate
+
+	// Iterate over bigger array & get smaller element
+
+	// Push smaller element to merged list
+
+	// Redo until any array gets empty
+
+	// Merge the other non empty array
+
+	// Return merged array
+
+	return []int{}
+}
