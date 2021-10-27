717055efef52cf7cc48bbc79897916e495b591d2
el10savio <el10savio96@gmail.com>
2021-10-27 14:37:55 +0530

    Added Merge Test Suite

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -6,6 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TODO: Move Test Suite
+// To Different File
+
 var testSortTestSuite = []struct {
 	name         string
 	list         []int
@@ -22,6 +25,30 @@ func TestSort(t *testing.T) {
 		t.Run(testCase.name, func(t *testing.T) {
 			t.Parallel()
 			actualList := Sort(testCase.list)
+			assert.Equal(t, testCase.expectedList, actualList)
+		})
+	}
+}
+
+var testMergeTestSuite = []struct {
+	name         string
+	list1        []int
+	list2        []int
+	expectedList []int
+}{
+	{"BasicFuntionality", []int{1, 3, 5}, []int{2, 3, 9}, []int{1, 2, 3, 3, 5, 9}},
+	{"List1Empty", []int{}, []int{2, 3, 9}, []int{2, 3, 9}},
+	{"List2Empty", []int{1, 3, 5}, []int{}, []int{1, 3, 5}},
+	{"BothListsEmpty", []int{}, []int{}, []int{}},
+}
+
+// TestMerge ...
+func TestMerge(t *testing.T) {
+	for _, testCase := range testMergeTestSuite {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			actualList := merge(testCase.list1, testCase.list2)
 			assert.Equal(t, testCase.expectedList, actualList)
 		})
 	}
