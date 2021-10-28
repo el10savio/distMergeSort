d2cf9d07b3d5ce59d6f967af51b3918e7faf4393
el10savio <el10savio96@gmail.com>
2021-10-28 21:48:11 +0530

    Disabled Sort Parallel Unit Test Runs

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -9,9 +9,7 @@ import (
 // TestSort ...
 func TestSort(t *testing.T) {
 	for _, testCase := range testSortTestSuite {
-		testCase := testCase
 		t.Run(testCase.name, func(t *testing.T) {
-			t.Parallel()
 			actualList := Sort(testCase.list)
 			assert.Equal(t, testCase.expectedList, actualList)
 		})
@@ -21,9 +19,7 @@ func TestSort(t *testing.T) {
 // TestMerge ...
 func TestMerge(t *testing.T) {
 	for _, testCase := range testMergeTestSuite {
-		testCase := testCase
 		t.Run(testCase.name, func(t *testing.T) {
-			t.Parallel()
 			actualList := merge(testCase.list1, testCase.list2)
 			assert.Equal(t, testCase.expectedList, actualList)
 		})
