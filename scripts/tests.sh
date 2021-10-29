787429a483caad1d82c7e17a19725c72edd2f680
el10savio <el10savio96@gmail.com>
2021-10-29 18:50:44 +0530

    Init e2e Tests

---
+++ scripts/tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Provisioning Cluster"
+scripts_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+bash ${scripts_dir}/provision.sh 3 > /dev/null
+
+echo "Cluster Sanity Tests"
+bats --tap ${scripts_dir}/bats/cluster-sanity.bats
+
+echo "Tearing Down Cluster"
+bash ${scripts_dir}/teardown.sh > /dev/null
+
