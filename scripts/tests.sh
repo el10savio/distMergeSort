8bf87e9ad651628f1c3d0c905c12fc563998da52
el10savio <el10savio96@gmail.com>
2021-10-31 17:45:46 +0530

    Shell Fixes

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,14 +2,13 @@
 
 echo "Provisioning Cluster"
 scripts_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-bash ${scripts_dir}/provision.sh 3 > /dev/null
+bash "${scripts_dir}"/provision.sh 3 >/dev/null
 
 echo "Cluster Sanity Tests"
-bats --tap ${scripts_dir}/bats/cluster-sanity.bats
+bats --tap "${scripts_dir}"/bats/cluster-sanity.bats
 
 echo "Sort Tests"
-bats --tap ${scripts_dir}/bats/sort.bats
+bats --tap "${scripts_dir}"/bats/sort.bats
 
 echo "Tearing Down Cluster"
-bash ${scripts_dir}/teardown.sh > /dev/null
-
+bash "${scripts_dir}"/teardown.sh >/dev/null
